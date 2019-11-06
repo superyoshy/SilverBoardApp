51c9315d6a98bb3038f80dfdb4e9990c2cc2ffd3
zwx <moyash@icloud.com>
2019-11-06 16:02:17 +0900

    スタートボタンタッチでホームシーンへ移動するようにした

---
+++ Assets/TitleScenes/Script/GameSystem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameSystem : MonoBehaviour
+{
+    // スタートボタンを押したら実行
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Home");
+    }
+}
