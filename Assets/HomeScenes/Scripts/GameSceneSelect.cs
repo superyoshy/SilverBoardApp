8ab7a1482d5113a40f266d15849ed123984cc838
zwx <moyash@icloud.com>
2019-11-15 16:29:23 +0900

    バトル画面、ホーム画面に移動するようにした

---
+++ Assets/HomeScenes/Scripts/GameSceneSelect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameSceneSelect : MonoBehaviour
+{
+    public GameObject BattlePanel;
+    public GameObject CharaSelectPanel;
+
+    // ホームボタンを押した時に実行
+    public void Onclickhome()
+    {
+        BattlePanel.SetActive(false);
+        CharaSelectPanel.SetActive(false);
+    }
+
+    // バトルボタンを押した時に実行
+    public void Onclickbattle()
+    {
+        BattlePanel.SetActive(true);
+        CharaSelectPanel.SetActive(false);
+    }
+
+    // キャラ選択ボタンを押した時に実行
+    public void Onclickcharaselect()
+    {
+        BattlePanel.SetActive(false);
+        CharaSelectPanel.SetActive(true);
+    }
+}
